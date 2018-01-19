7f0b690d88646090d03dbc1ad56de0c2c3554046
Antoine Laurent <antoine@famille-laurent.com>
2018-01-19 11:08:46 -0500

    awesome: randr delete cause of wallpaper issue
    when starting, putting the screen off and on caused screnn to loose the
    wallpaper.
    fix could be to call .fehbg after changing on/off

--- awesome/rc/auto.lua
+++ awesome/rc/auto.lua
@@ -10,7 +10,7 @@ run_once("xrdb ~/.Xresources")
 run_once("xcompmgr -c -C -t-5 -l-5 -r4.2 -o.55 &")
 run_once(redshift.." & disown")
 
-if screen:count() == 1 then
-    run_once("xrandr --output eDP1 --off ; xrandr --output eDP1 --auto")
-end
+-- if screen:count() == 1 then
+--     run_once("xrandr --output eDP1 --off ; xrandr --output eDP1 --auto")
+-- end
 -- }}}
