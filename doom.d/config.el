647a24c40cd2171de36265239c5109bf30a1a105
Antoine Laurent <antoine@famille-laurent.com>
2026-04-07 21:49:30 -0400

    fix: update mail

--- doom.d/config.el
+++ doom.d/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Antoine Laurent"
-      user-mail-address "alaurent@agencemobilitedurable.ca")
+      user-mail-address "antoine@famille-laurent.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
